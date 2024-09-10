3f55521f26bdd9eb9735ce638d6045ccc3de72e0
Fabian Hanso <f.hanso@revice-media.com>
2024-09-10 10:57:14 +0200

    Set filter for news if Slug is given If not, news is not displayed

--- components/News/NewsOverview.tsx
+++ components/News/NewsOverview.tsx
@@ -31,51 +31,54 @@ export default function NewsOverview({ showHeadline, data }: NewsProps) {
           </div>
         )}
         <div className="mx-auto mt-8 grid max-w-2xl grid-cols-1 gap-x-6 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {data.map((item: any) => (
-            <a
-              key={item.fields.domainSlug}
-              href={
-                item.sys.contentType.sys.id === "spielberichte"
-                  ? `/verein/spielberichte/${item.fields.domainSlug}`
-                  : `/verein/news/${item.fields.domainSlug}`
-              }
-              className="group flex flex-col items-start justify-between"
-            >
-              <div className="relative w-full">
-                <img
-                  src={
-                    item.fields.image?.fields.file.url ||
-                    "/images/volleyball.jpg"
+          {data.map(
+            (item: any) =>
+              item.fields.domainSlug && (
+                <a
+                  key={item.fields.domainSlug}
+                  href={
+                    item.sys.contentType.sys.id === "spielberichte"
+                      ? `/verein/spielberichte/${item.fields.domainSlug}`
+                      : `/verein/news/${item.fields.domainSlug}`
                   }
-                  alt=""
-                  className="aspect-[16/9] w-full rounded-xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2] group-hover:grayscale"
-                />
-              </div>
-              <div className="max-w-xl">
-                <div className="mt-4 flex items-center gap-x-4 text-xs">
-                  <time dateTime="" className="text-gray-500">
-                    {item.fields.date}
-                  </time>
-                  <div className="relative rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 group-hover:bg-gray-100">
-                    {item.fields.author}
+                  className="group flex flex-col items-start justify-between"
+                >
+                  <div className="relative w-full">
+                    <img
+                      src={
+                        item.fields.image?.fields.file.url ||
+                        "/images/volleyball.jpg"
+                      }
+                      alt=""
+                      className="aspect-[16/9] w-full rounded-xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2] group-hover:grayscale"
+                    />
                   </div>
-                </div>
-                <div className="group relative">
-                  <h3 className="mt-2 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                    <div>
-                      <span className="absolute inset-0" />
-                      {item.fields.headline}
+                  <div className="max-w-xl">
+                    <div className="mt-4 flex items-center gap-x-4 text-xs">
+                      <time dateTime="" className="text-gray-500">
+                        {item.fields.date}
+                      </time>
+                      <div className="relative rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 group-hover:bg-gray-100">
+                        {item.fields.author}
+                      </div>
                     </div>
-                  </h3>
-                  {/* {item.fields.previewText && (
+                    <div className="group relative">
+                      <h3 className="mt-2 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                        <div>
+                          <span className="absolute inset-0" />
+                          {item.fields.headline}
+                        </div>
+                      </h3>
+                      {/* {item.fields.previewText && (
                     <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
                       {item.fields.previewText}
                     </p>
                   )} */}
-                </div>
-              </div>
-            </a>
-          ))}
+                    </div>
+                  </div>
+                </a>
+              )
+          )}
         </div>
       </div>
     </div>
